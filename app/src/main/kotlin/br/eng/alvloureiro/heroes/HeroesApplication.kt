4da9898946516970de313d5cd0b54b290f343991
Andre Loureiro <loureiro.andrew@gmail.com>
2018-07-21 14:18:21 -0400

    [Chore]: Add a base implementation to `HeroesApplication` class
    Signed-off-by: Andre Loureiro <loureiro.andrew@gmail.com>

--- app/src/main/kotlin/br/eng/alvloureiro/heroes/HeroesApplication.kt
+++ app/src/main/kotlin/br/eng/alvloureiro/heroes/HeroesApplication.kt
@@ -1,8 +1,25 @@
 package br.eng.alvloureiro.heroes
 
 import android.app.Application
+import br.eng.alvloureiro.heroes.di.components.ApplicationComponent
+import br.eng.alvloureiro.heroes.di.components.DaggerApplicationComponent
+import br.eng.alvloureiro.heroes.di.modules.ApplicationModule
+import br.eng.alvloureiro.heroes.di.modules.NetworkModule
 
 
 class HeroesApplication: Application() {
+
+    private val component: ApplicationComponent? by lazy {
+        DaggerApplicationComponent.builder().networkModule(NetworkModule(this))
+            .applicationModule(ApplicationModule(this))
+            .build()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        component?.inject(this)
+    }
+
+    fun component() = component
 
 }
